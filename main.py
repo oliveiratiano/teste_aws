aaa698bb4bb7c7dfbea242e8869491c167b189c5
oliveiratiano <oliveiratiano@gmail.com>
2021-04-17 11:56:59 -0300

    programa básico

---
+++ main.py
@@ -0,0 +1,6 @@
+
+import subprocess
+cmd = ["bash", "glove/demo.sh"]
+treinar_glove = subprocess.Popen(cmd,  stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output, errors = treinar_glove.communicate()
+treinar_glove.wait()
