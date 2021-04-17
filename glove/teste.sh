03fb5397c6bfeee9eee3e7a5146ac827e18872bd
oliveiratiano <oliveiratiano@gmail.com>
2021-04-17 12:28:29 -0300

    Create teste.sh

---
+++ glove/teste.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Makes programs, downloads sample data, trains a GloVe model, and then evaluates it.
+# One optional argument can specify the language used for eval script: matlab, octave or [default] python
+
+make
+
+echo bobo
