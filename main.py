c32bced56d9ce287d6cdd57ec5fd4f292bcb5bfb
oliveiratiano <oliveiratiano@gmail.com>
2021-04-17 13:15:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import stat
 st = os.stat('teste.sh')
 os.chmod('teste.sh', st.st_mode | stat.S_IEXEC)
-cmd = ["./teste.sh", "AQUI", "ALI"]
+cmd = ["bash", "./teste.sh", "AQUI", "ALI"]
 treinar_glove = subprocess.Popen(cmd,  stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 output, errors = treinar_glove.communicate()
 print(output)
