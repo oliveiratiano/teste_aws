77c0dc927caebd0428672ce8fce33bb8b15e48a0
oliveiratiano <oliveiratiano@gmail.com>
2021-04-17 12:52:23 -0300

    asd

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 
 import subprocess
-cmd = ["./teste.sh", "varum", "vardois"]
+cmd = ["./src/teste.sh"]
 treinar_glove = subprocess.Popen(cmd,  stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd="glove")
 output, errors = treinar_glove.communicate()
 print(output)
 print(errors)
-import pdb; pdb.set_trace()
 treinar_glove.wait()
--- src/teste.sh
+++ src/teste.sh
@@ -4,10 +4,8 @@ set -e
 # Makes programs, downloads sample data, trains a GloVe model, and then evaluates it.
 # One optional argument can specify the language used for eval script: matlab, octave or [default] python
 
-make
-
 
 CORPUS=$1
 VOCAB_FILE=$2
 
-echo $CORPUS e VOCAB_FILE
+echo $CORPUS e $VOCAB_FILE
