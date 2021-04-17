e4a2acb2addd02b13856a87c737c26f869fd7b86
oliveiratiano <oliveiratiano@gmail.com>
2021-04-17 13:13:47 -0300

    Update teste.sh

--- src/teste.sh
+++ src/teste.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-cd glove
 
 # Makes programs, downloads sample data, trains a GloVe model, and then evaluates it.
 # One optional argument can specify the language used for eval script: matlab, octave or [default] python
