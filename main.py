f9aa34a0cd8a532a920efdcdb1558ab043de34aa
oliveiratiano <oliveiratiano@gmail.com>
2021-04-17 18:12:48 -0300

    sadf

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 import subprocess
 import os
 import stat
-st = os.stat('glove/demo.sh')
-os.chmod('glove/demo.sh', st.st_mode | stat.S_IEXEC)
-cmd = ["./demo.sh"]
+st = os.stat('glove/glove.sh')
+os.chmod('glove/glove.sh', st.st_mode | stat.S_IEXEC)
+cmd = ["./glove.sh", "var_um", "var_dois", "var_tres", "var_quatro", "var_cinco", "var_seis"]
 treinar_glove = subprocess.Popen(cmd,  stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd='glove')
 output, errors = treinar_glove.communicate()
-print(output)
-print(errors)
 treinar_glove.wait()
-print(output)
-print(errors)
+print('output:')
+print(output.decode('utf-8'))
+print('errors:')
+print(errors.decode('utf-8'))
