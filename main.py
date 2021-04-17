0a98c3070d54c763e214b55d93951abe656eb7f4
oliveiratiano <oliveiratiano@gmail.com>
2021-04-17 12:26:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import subprocess
-cmd = ["cd glove","./demo.sh"]
-treinar_glove = subprocess.Popen(cmd,  stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+cmd = ["./demo.sh"]
+treinar_glove = subprocess.Popen(cmd,  stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd="glove")
 output, errors = treinar_glove.communicate()
 treinar_glove.wait()
 print(output)
